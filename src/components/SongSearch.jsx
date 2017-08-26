0d86121201c0126dba4a3f0387221d4f5f0b0c89
tcsuder <tcsuder@gmail.com>
2017-08-26 11:36:47 -0700

    log search from inline handler

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import styles from "./../styles/SongSearch.css";
 
 function SongSearch(){
+  let input;
+
   return (
     <div className = {styles.songSearch}>
-      <form>
-        <input placeholder="Song Title"></input>
+      <form onSubmit={e => {
+        e.preventDefault();
+        if (!input.value.trim()) {
+          return;
+        }
+
+        // this is where custom logic goes
+        console.log("SEARCHED ARTIST:");
+        console.log(input.value.trim());
+        //
+
+        input.value = "";
+      }}>
+        <input placeholder="Song Title" ref={node => {
+          input = node;
+        }}></input>
         <button>search</button>
       </form>
     </div>
