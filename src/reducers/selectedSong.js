f1df46a955457687dcc1782d3a5cf9f0af95972c
tcsuder <tcsuder@gmail.com>
2017-08-27 12:36:28 -0700

    pass test to change selected song

--- src/reducers/selectedSong.js
+++ src/reducers/selectedSong.js
@@ -5,10 +5,10 @@ const selectedSong = (state = defaultState.selectedSong, action) => {
   let newState;
   switch (action.type) {
     case types.SELECT_SONG:
-      // newState = Object.assign({}, state, {
-      //   selectedSong: action.artistTitleString
-      // });
-      // return newState;
+      newState = Object.assign({}, state, {
+        selectedSong: action.songId
+      });
+      return newState;
     default:
       return state;
   }
