4c7c46c55615bdec1a45e292bea674a024c0dcbf
tcsuder <tcsuder@gmail.com>
2017-08-27 12:17:44 -0700

    restructure testing file

--- __tests__/reducers/index.js
+++ __tests__/reducers/index.js
@@ -1,20 +1,26 @@
 import constants from "./../../src/constants";
-import reducer from "./../../src/reducers";
+import * as actions from "./../../src/actions";
+import selectedSong from "./../../src/reducers/selectedSong";
+import songsById from "./../../src/reducers/songsById";
 
-describe("Karaoke Reducer", () => {
+describe("Karaoke App", () => {
   const { defaultState, types } = constants;
 
-  it('should return default state', () => {
-    expect(reducer(defaultState, { type: null })).toEqual(defaultState);
-  });
+  describe("Songs By Title Reducer", () => {
 
-  it("should update chorus phrase", () => {
-    expect(reducer(defaultState, { type: types.NEXT_LINE }).currentPhrase).toEqual(defaultState.chorusArray[1]);
-  });
+    // it('should return default state', () => {
+    //   expect(songsById(defaultState.songsById, { type: null })).toEqual("test");
+    // });
 
-  it("should restart song", () => {
-    const newState = reducer(defaultState, { type: types.NEXT_LINE });
-    expect(reducer(newState, { type: types.RESTART_SONG })).toEqual(defaultState);
+    // it("should update chorus phrase", () => {
+    //   expect(reducer(defaultState, { type: types.NEXT_LINE }).currentPhrase).toEqual(defaultState.chorusArray[1]);
+    // });
+    //
+    // it("should restart song", () => {
+    //   const newState = reducer(defaultState, { type: types.NEXT_LINE });
+    //   expect(reducer(newState, { type: types.RESTART_SONG })).toEqual(defaultState);
+    // });
+
   });
 
 });
