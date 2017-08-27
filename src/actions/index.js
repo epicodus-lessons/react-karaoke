e5b8d16cfa9359226ed43aa37b93f2d08ecf2b98
tcsuder@gmail.com <tcsuder@gmail.com>
2017-08-27 16:15:48 -0700

    build actions up to point of making call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import * as types from "./../constants/ActionTypes";
+import v4 from "uuid/v4";
 
 export const nextLine = (songId) => ({
   type: types.NEXT_LINE,
@@ -25,5 +26,23 @@ export const recieveSong = (title, artist, json) => ({
 export const requestSong = (title, artist) => ({
   type: types.REQUEST_SONG,
   title,
-  artist
+  artist,
+  songId: v4()
 });
+
+export function fetchLyrics(title, artist) {
+  return function (dispatch) {
+    dispatch(requestSong(title, artist));
+    return fetch("http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&q_track=${title}&page_size=1&s_track_rating=desc&apikey=a8503c69d6322a8e9e7faaaa8afc05a1")
+    .then(
+      response => response.json(),
+      error => console.log("An error occured.", error)
+    )
+    .then(jason => (
+        // have to make extra call here to get lyrics with response.track_id
+        console.log(json)
+
+      )
+    )
+  }
+}
